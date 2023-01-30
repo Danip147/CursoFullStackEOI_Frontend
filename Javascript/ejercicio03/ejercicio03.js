4c9cc80b23c166c23c4a09a56f8ffae998708d42
Dani <danielporras147@outlook.es>
2023-01-30 14:23:33 +0100

    Ejercicio 03

---
+++ Javascript/ejercicio03/ejercicio03.js
@@ -0,0 +1,8 @@
+let x = 5;
+let y = 8;
+
+console.log(++x * y--); // x=6  Y=7  resultado = 48
+
+console.log(x-- - y--); // x= 5  y=6 resultado = -1
+
+console.log(--x + y++); // x 4  y= 7 resultado= 10;
